5dc1b4ee18c708365d327d680500cf875d504ce5
diego <755232@unizar.es>
2021-10-28 22:50:01 +0200

    testing conexion to heroku db

--- src/db/conexion.js
+++ src/db/conexion.js
@@ -1,12 +1,29 @@
-
-let localDBurl = "postgres://postgres:postgres@localhost:5432"
+/*
+ * conexion.js connects to a postgres database using the ORM sequelize.
+ * If node server is runned locally, sequelize will connect to a local db.
+ *      IMPORTANT: Execute previusly `npm run startdb` to run a docker with postgres
+ * If node server is runned in Heroku, sequelize will connect to heroku db.
+ */
 
 const Sequelize = require('sequelize');
-sequelize = new Sequelize(process.env.DATABASE_URL || localDBurl, {
-        dialectOptions: {}
-    }
-);
+let productionDBurl = process.env.DATABASE_URL
 
+if (productionDBurl != null) { //Conexion with production db
+    sequelize = new Sequelize(productionDBurl || localDBurl, {
+            dialectOptions: {
+                ssl: {
+                    require: true,
+                    rejectUnauthorized: false
+                }
+            }
+        }
+    );
+} else { //Conexion with local db
+    sequelize = new Sequelize("postgres://postgres:postgres@localhost:5432", {
+            dialectOptions: {}
+        }
+    );
+}
 sequelize
     .authenticate()
     .then(() => {
