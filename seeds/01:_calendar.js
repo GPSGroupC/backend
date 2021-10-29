e19299d0780272c798516bad917b7cea582aeda8
diego <755232@unizar.es>
2021-10-29 23:50:46 +0200

    Clean migration files

--- seeds/01:_calendar.js
+++
@@ -1,7 +0,0 @@
-
-exports.seed = function(knex) {
-  // Deletes ALL existing entries
-    let createQuery =
-        `INSERT INTO Calendar (type) VALUES ('grados')`
-    return knex.raw(createQuery)
-};
