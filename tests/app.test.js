13b431e13c52deb64abeee6e9ea511a11a0b2f2b
Sergio Benitez Mombiela <755787@unizar.es>
2021-11-04 23:07:46 +0100

    test GET /calendar/getCalendar

--- tests/app.test.js
+++ tests/app.test.js
@@ -38,4 +38,25 @@ describe('Testing rest API', () =>{
         })
         
     })
+
+    it('GET /calendar/getCalendar devuelve los calendarios de un curso solicitado ', function(done) {
+        const data = {
+            tipo:"Grado",
+            course:"2049-2050",
+            fecha_inicio_1:"09-09-2049",
+            fecha_inicio_2:"09-02-2050",
+            convSeptiembre:"09-09-2050"
+        }
+        request(app)
+        .post('/calendar/updateCalendar')
+        .send(data)
+        .end(function(){
+            request(app)
+            .get('/calendar/getCalendar')
+            .query({course: "2049-2050"})
+            .expect('Content-Type', 'application/json; charset=utf-8')
+            .expect(200, done);
+            //Falta deletear el calendario creado para dejar la base de datos como estaba
+        })
+    })
 })
